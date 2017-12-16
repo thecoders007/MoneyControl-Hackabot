17eaadd3157946421e54d3b87f2a9bb5464824d7
ketavbhatt <ketavbhatt@gmail.com>
2017-12-16 22:14:11 +0530

    main page views

--- financial_modeling_tool/stock_prediction/views.py
+++ financial_modeling_tool/stock_prediction/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import login as auth_login
 from django.http import HttpResponse
+from nasdaq_stock_quote import Share
+import requests
+import json
 
 
 # Create your views here.
@@ -63,7 +66,64 @@ def logout(request):
 
 
 def main(request):
-	return render(request,'mainpage.html')
+	a =['AAPL','MSFT','YHOO'] 
+	# a_prev = []
+	# a_volume = []
+	# a_perct = []
+	# a_high = []
+	# a_low = []
+	# a_price = []
+	# for i in a:
+	# 	share = Share(i)
+	# 	print share,"asdasdads"
+	# 	prev = share.get_prev_close()
+	# 	price = share.get_price()
+	# 	name = share.get_name()
+	# 	volume = share.get_volume()
+	# 	perct = share.get_percent_change()
+	# 	high = share.get_day_high()
+	# 	low = share.get_day_low()
+	# 	perct = float(perct)
+	# 	perct = perct*100
+	# 	perct = str(perct)
+	# 	print prev
+	# 	# a_prev.append(prev)
+	# 	# a_volume.append(volume)
+	# 	# a_perct.append(perct)
+	# 	# a_high.append(high)
+	# 	# a_low.append(low)
+	# 	print name
+
+	# print a_prev
+	# loop = zip(a,a_prev,a_volume,a_price,a_low,a_high,a_perct)
+	# print loop
+
+	prev = []
+	high = []
+	low = []
+	close = []
+	volume = []
+
+	for i in a:
+			r = requests.get('https://www.quandl.com/api/v3/datasets/EOD/AAPL.json?api_key=oqf4vFLPo8MrPBGXVjki')
+			stock_json = r.json()
+			# s_json.append(stock_json)
+			print stock_json['dataset']['data'][0]
+			prev.append(stock_json['dataset']['data'][0][1])
+			high.append(stock_json['dataset']['data'][0][2])
+			low.append(stock_json['dataset']['data'][0][3])
+			close.append(stock_json['dataset']['data'][0][4])
+			volume.append(stock_json['dataset']['data'][0][5])
+
+	# print s_json
+
+	# print stock_json
+
+	# s_json.encode("utf-8")
+	loop = zip(a,prev,high,low,close,volume)
+	print loop
+
+	return render(request,'mainpage.html',{'loop' : loop})
 
 
 def classes(request):
