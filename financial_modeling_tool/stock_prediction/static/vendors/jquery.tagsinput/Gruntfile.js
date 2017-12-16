ebc8269c4acc69b424ef92c5116e49fba122b7d0
ketavbhatt <ketavbhatt@gmail.com>
2017-12-16 14:04:35 +0530

    template and static files

---
+++ financial_modeling_tool/stock_prediction/static/vendors/jquery.tagsinput/Gruntfile.js
@@ -0,0 +1,64 @@
+<<<<<<< HEAD
+function loadConfig(path) {
+   var glob = require('glob');
+   var object = {};
+   var key;
+
+   glob.sync('*', {cwd: path}).forEach(function(option) {
+      key = option.replace(/\.js$/,'');
+      object[key] = require(path + option);
+   });
+
+  return object;
+}
+
+module.exports = function(grunt) {
+   grunt.loadTasks('grunt-tasks');
+
+   require('time-grunt')(grunt);
+   
+   // Only load tasks when they are needed
+   require('jit-grunt')(grunt, {
+      ngtemplates: 'grunt-angular-templates'
+   });
+
+   var config = {
+      pkg: grunt.file.readJSON('package.json'),
+      env: process.env
+   };
+
+   grunt.util._.extend(config, loadConfig('./grunt-tasks/options/'));
+   grunt.initConfig(config);
+=======
+function loadConfig(path) {
+   var glob = require('glob');
+   var object = {};
+   var key;
+
+   glob.sync('*', {cwd: path}).forEach(function(option) {
+      key = option.replace(/\.js$/,'');
+      object[key] = require(path + option);
+   });
+
+  return object;
+}
+
+module.exports = function(grunt) {
+   grunt.loadTasks('grunt-tasks');
+
+   require('time-grunt')(grunt);
+   
+   // Only load tasks when they are needed
+   require('jit-grunt')(grunt, {
+      ngtemplates: 'grunt-angular-templates'
+   });
+
+   var config = {
+      pkg: grunt.file.readJSON('package.json'),
+      env: process.env
+   };
+
+   grunt.util._.extend(config, loadConfig('./grunt-tasks/options/'));
+   grunt.initConfig(config);
+>>>>>>> 5533bdf65d878a145647c5a627c74d872d536ee2
+};
---
+++ financial_modeling_tool/stock_prediction/static/vendors/jqvmap/grunt/index.js
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+var requireDirectory = require("require-directory");
+=======
+var requireDirectory = require("require-directory");
+>>>>>>> 5533bdf65d878a145647c5a627c74d872d536ee2
+module.exports = requireDirectory(module);
