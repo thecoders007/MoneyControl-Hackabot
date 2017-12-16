f3b0092e7d9b86acda18ff2db90783d9859926a5
ketavbhatt <ketavbhatt@gmail.com>
2017-12-16 15:56:26 +0530

    login and register

--- financial_modeling_tool/stock_prediction/views.py
+++ financial_modeling_tool/stock_prediction/views.py
@@ -1,17 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import logout as auth_logout
+from django.http import HttpResponse
+
 
 # Create your views here.
 
 def register(request):
-	return render(request,'register.html')
+	if request.method == 'POST':
+		name = request.POST['name']
+		email = request.POST['email']
+		password1 = request.POST['password1']
+		password2 = request.POST['password2']
+
+
+		
+		if password1 != password2:
+			return HttpResponse("Enter Password Correctly")
+			
+		user = User.objects.create(username=email,first_name=name,email=email)
+		user.set_password(password1)
+		
+		
+		user.save()
+		user = authenticate(username = email, password = password1)
+		if user:
+			login(user)
+			return redirect('/main/')
+		
+	else:
+
+		return render(request,"register.html")
+
+
 
 
 def login(request):
-	return render(request,'login.html')
+	if request.method == 'POST':
+		email = request.POST['email']
+		password = request.POST['password']
+		user = authenticate(username = email, password = password)
+		if user:
+			login(request)
+			return redirect('/main/')
+		else:
+			return render(request, 'login.html',{'context' : 'Wrong username or password'})
+
+	else:	
+		return render(request, 'login.html')
+	
 
 
 def logout(request):
-	return render(request)
+	if request.user.is_authenticated():
+		auth_logout(request)
+	else:
+		return HttpResponse("invalid")
+	return render(request,'login.html')
+	
 
 
 def main(request):
